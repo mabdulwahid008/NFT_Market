b235bd347cc71955afc9594081c08352e4d082c7
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-04-30 14:37:03 +0500

    build rest of the contract methods

--- nft-hardhat/contracts/NFTMarket.sol
+++ nft-hardhat/contracts/NFTMarket.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 struct NFTListing{
     uint256 price;
     address seller;
 }
 
-contract NFTMarket is ERC721URIStorage{
+contract NFTMarket is ERC721URIStorage, Ownable{
     using SafeMather for uint256;
 
     using Counters for Counters.Counter;
@@ -20,6 +21,10 @@ contract NFTMarket is ERC721URIStorage{
  
     constructor() ERC721("Muhammad Abdulwahid", "MAW") {}
 
+    // if tokenURI is not an empty string => NFT created
+    // if price is not 0 => NFT listed for sale
+    // if price is 0 && tokenURI is empty => NFT transfered (either bought or cancel listing)
+    event NFTTransfer(uint256 tokenID, address to, string tokenURI, uint256 price);
 
     // Create NFT
     function createNFT(string calldata tokenURI) public{
@@ -27,6 +32,8 @@ contract NFTMarket is ERC721URIStorage{
         uint256 currentID = _tokenIDs.current();
         _safeMint(msg.sender, currentID);
         _setTokenURI(currentID, tokenURI);
+
+        emit NFTTransfer(currentID, msg.sender, tokenURI, 0);
     }
 
     // List NFT
@@ -36,6 +43,8 @@ contract NFTMarket is ERC721URIStorage{
         approve(address(this), tokenID);
         transferFrom(msg.sender, address(this), tokenID);
         _listings[tokenID] = NFTListing(price, msg.sender);
+
+        emit NFTTransfer(tokenID, address(this), "", price);
     }
 
     // Buy NFT
@@ -46,10 +55,32 @@ contract NFTMarket is ERC721URIStorage{
         transferFrom(address(this), msg.sender, tokenID);
         payable(msf.sender.transfer(listing.price.mul(95).div(100)));
         // payable(listing.address.transfer(listing.price.mul(95).div(100))); // price * 95 / 100; deducting 5% of the price as fee
+        clearListing(tokenID);
+
+        emit NFTTransfer(tokenID, msg.sender, "", 0);
     }
 
-    // Cancel List
-    function cancelList(uint256 tokenID) public {
+    // Cancel Listing
+    function cancelListing(uint256 tokenID) public {
+        NFTListing memory listing = _listings[tokenID];
+        require(listing.price > 0, "NFTMarket: NFT not listed for sale");
+        require(listing.address == msg.sender, "NFTMarket: You are not the owner of NFT");
+        transferFrom(address(this), msg.sender, tokenID);
+        clearListing(tokenID);
         
+        emit NFTTransfer(tokenID, msg.sender, "", 0);
+    }
+
+    // Clear Lisiting from struct
+    function clearListing(uint256 tokenID) private {
+        _listings[tokenID].price = 0;
+        _listings[tokenID].address = address(0);
+    }
+
+    // Withdraw funds
+    function withdrawFunds() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "NNFTMarket: Balance is 0");
+        payable(owner()).transfer(balance);
     }
 }
